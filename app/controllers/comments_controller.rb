cd0c121fa88d54d6a4c67964603c5bf68541d885
Tim Booher <tim@theboohers.org>
2013-05-18 12:57:11 -0400

    updated

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
   # GET /comments
   # GET /comments.json
   def index
-    @comments = Comment.all
+    @comments = Comment.desc(:created_at)
 
     respond_to do |format|
       format.html # index.html.erb
